baed17514bc9ff5134451eccbc60f7975175e568
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-30 20:10:43 +0200

    Making some changes to modified my portfolio

--- src/Constant/Constant.js
+++ src/Constant/Constant.js
@@ -1,8 +1,15 @@
 const navLinks = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-  { href: "/service", label: "Service" },
-  { href: "/work", label: "Work" },
-  { href: "/contact", label: "Contact" },
+	{ href: "/", label: "Home" },
+	{ href: "/about", label: "About" },
+	{ href: "/service", label: "Service" },
+	{ href: "/work", label: "Work" },
+	{ href: "/contact", label: "Contact" },
 ];
 export default navLinks;
+
+export const ABOUT_ME = ` Hello there! My name is Oplano James Mulbah. I am a web developer
+& Software Engineer, and I&apos;m very passionate and dedicated to
+my work.   With +6 months experience as a professional a wed developer, I
+have acquired the skills and knowledge necessary to make your
+project a success. I enjoy every step of the design process, from
+discussion and collaboration.`;
