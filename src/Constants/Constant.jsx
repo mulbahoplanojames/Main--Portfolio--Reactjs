5b246a789947251eb57e441a9c08ae2d6fdebf97
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-30 20:07:24 +0200

    Making some changes to modified my portfolio

---
+++ src/Constants/Constant.jsx
@@ -0,0 +1,6 @@
+const ABOUT_ME = ` Hello there! My name is Oplano James Mulbah. I am a web developer
+& Software Engineer, and I&apos;m very passionate and dedicated to
+my work.   With +6 months experience as a professional a wed developer, I
+have acquired the skills and knowledge necessary to make your
+project a success. I enjoy every step of the design process, from
+discussion and collaboration.`;
