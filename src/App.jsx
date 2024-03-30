cf3f3a29c9a89533c0536abcecfd9d273b198b51
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-30 20:19:08 +0200

    Making some changes to modified my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,5 @@
-import React from "react";
-import Navbar from "./Components/NavigationBar/Navbar";
-import Hero from "./Components/Hero/Hero";
-import About from "./Components/About/About";
-import Service from "./Components/Service/Service";
-import Work from "./Components/Work/Work";
-import Contactinfo from "./Components/ContactInfo/Contactinfo";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Service />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/contact" element={<Contactinfo />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+	return <div>App</div>;
 };
 
 export default App;
