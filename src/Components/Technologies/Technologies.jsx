e0231c0b34964a36d18a0c09ac95ae5c8043c46f
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-31 12:56:08 +0200

    Added the Technologies section as a component to the project to indicated technologies that i am familar with and an effect to the scrolllbar

--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -1,8 +1,20 @@
+import { FaHtml5, FaCss3Alt, FaReact, FaGitAlt } from "react-icons/fa";
+import { BiLogoJavascript, BiLogoTailwindCss } from "react-icons/bi";
+
 const Technologies = () => {
 	return (
 		<>
 			<div className='mt-24 text-center mb-80'>
-				<h1 className=''>Technologies</h1>
+				<h1 className='text-4xl pb-8'>Technologies</h1>
+
+				<div className='flex flex-wrap gap-6 justify-center items-center'>
+					<div className='TechIconWripper'></div>
+					<div className='TechIconWripper'></div>
+					<div className='TechIconWripper'></div>
+					<div className='TechIconWripper'></div>
+					<div className='TechIconWripper'></div>
+					<div className='TechIconWripper'></div>
+				</div>
 			</div>
 		</>
 	);
--- src/Constant/Constant.js
+++ src/Constant/Constant.js
@@ -1,3 +1,5 @@
+
+
 export const navLinks = [
 	{ id: 1, href: "/" },
 	{ id: 2, href: "/about" },
@@ -13,3 +15,21 @@ project a success. I enjoy every step of the design process, from
 discussion and collaboration. `;
 
 export default ABOUT_ME;
+
+// const Html5 = <FaHtml5 className='' />;
+// const Css3 = <FaCss3Alt className='' />;
+// const Javascript = <BiLogoJavascript className='' />;
+// const React = <FaReact className='' />;
+// const Tailwind = <BiLogoTailwindCss className='' />;
+// const Git = <FaGitAlt className='' />;
+
+// const TechnologiesIcons = [
+// 	{ id: 1, icon: Html5 },
+// 	{ id: 2, icon: Css3 },
+// 	{ id: 3, icon: Javascript },
+// 	{ id: 4, icon: React },
+// 	{ id: 5, icon: Tailwind },
+// 	{ id: 6, icon: Git },
+// ];
+
+// export default TechnologiesIcons;
