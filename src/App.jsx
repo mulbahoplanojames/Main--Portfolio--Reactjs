0a1627a3f285c018ecbd6bc38a7e2f3cbdf67171
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-31 12:04:56 +0200

    Added the Technologies section as a component to the project to indicated technologies that i am familar with

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
-// import Technologies from "./Components/Technologies/Technologies";
+import Technologies from "./Components/Technologies/Technologies";
 
 const App = () => {
 	return (
@@ -10,7 +10,7 @@ const App = () => {
 			{/* // !This is to give the below components a fix width to be display in the middle */}
 			<div className='container mx-auto md:px-10 px-2	mt-40'>
 				<Hero />
-				{/* <Technologies /> */}
+				<Technologies />
 			</div>
 		</>
 	);
--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -1,11 +1,11 @@
-// const Technologies = () => {
-// 	return (
-// 		<>
-// 			<div className=''>
-// 				<h1 className=''>Technologies</h1>
-// 			</div>
-// 		</>
-// 	);
-// };
+const Technologies = () => {
+	return (
+		<>
+			<div className=''>
+				<h1 className=''>Technologies</h1>
+			</div>
+		</>
+	);
+};
 
-// export default Technologies;
+export default Technologies;
