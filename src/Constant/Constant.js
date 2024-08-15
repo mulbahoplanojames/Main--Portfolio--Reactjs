6c2caf737453184df7118d790aeed1121c9a126b
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-08-15 14:18:46 +0200

    Added some newly create projects

--- src/Constant/Constant.js
+++ src/Constant/Constant.js
@@ -1,4 +1,6 @@
 import DigitStemImg from "/src/assets/digitstem.png";
+import Naa_kai_img from "/src/assets/Naa-kai.png";
+import Dorcas_school from "/src/assets/borcas_school.png";
 import DevTechImg from "/src/assets/Dev!Tech_Blog.png";
 import BoldImg from "/src/assets/Bold.png";
 import Math4kidsImg from "/src/assets/math4kids.png";
@@ -12,57 +14,87 @@ project a success. I enjoy every step of the design process, from
 discussion and collaboration. `;
 
 export const PROJECTS = [
-	{
-		projectID: 1,
-		image: DevTechImg,
-		projectTitle: "Dev!Tech Blog and Ebooks",
-		projectDescription:
-			"A comprehensive blogging platform focusing on technology-related topics. Users can explore a wide range of ebooks and blogs. It provides a user-friendly interface to navigate through different sections of the web App. It also as a feature where users access OpenAi.",
-		technologiesUsed: ["React", "Tailwind Css"],
-		liveDemo: "https://devtech-ebook-blog.netlify.app/",
-		code: "https://github.com/mulbahoplanojames/Dev-Tech-Ebook---Blog",
-	},
-	{
-		projectID: 2,
-		image: BoldImg,
-		projectTitle: "Boldo Company website",
-		projectDescription:
-			"This Project is for a web application the allow client and to unit with other business partners to buy products and view some of the latest blog posts..",
-		technologiesUsed: ["React", "Vanilla CSS", "JavaScript"],
-		liveDemo: "https://boldo-com.vercel.app/",
-		code: "https://github.com/mulbahoplanojames/Boldo.com",
-	},
-	{
-		projectID: 3,
-		image: DigitStemImg,
-		projectTitle: "DigitStem Clone Landing Page",
-		projectDescription:
-			"This project is a Front-end clone of the Digitstem platform that connects vendors to buyers through active affiliates. It offers various features and services aimed at facilitating online business transactions.",
-		technologiesUsed: ["HTML", "Vanilla CSS", "JavaScript"],
-		liveDemo: "https://mulbahoplanojames.github.io/Digitstem-Clone/",
-		code: "https://github.com/mulbahoplanojames/Digitstem-Clone",
-	},
-	{
-		projectID: 4,
-		image: Math4kidsImg,
-		projectTitle: "Math 4 Kids, a math game for Kids",
-		projectDescription:
-			"This is a web application designed to help kids learn Mathematics in a fun and interactive way. This project makes kids to learn mathematics in an easy and simple way. The project utilizes HTML, CSS, and JavaScript.",
-		technologiesUsed: ["HTML", "Vanilla CSS", "Vanilla JavaScript"],
-		liveDemo: "https://mulbahoplanojames.github.io/Math-4-Kids/",
-		code: "https://github.com/mulbahoplanojames/Math-4-Kids",
-	},
+  {
+    projectID: 1,
+    image: DevTechImg,
+    projectTitle: "Dev!Tech Blog and Ebooks",
+    projectDescription:
+      "A comprehensive blogging platform focusing on technology-related topics. Users can explore a wide range of ebooks and blogs. It provides a user-friendly interface to navigate through different sections of the web App. It also as a feature where users access OpenAi.",
+    technologiesUsed: ["React", "Tailwind Css"],
+    liveDemo: "https://devtech-ebook-blog.netlify.app/",
+    code: "https://github.com/mulbahoplanojames/Dev-Tech-Ebook---Blog",
+  },
+  {
+    projectID: 2,
+    image: Naa_kai_img,
+    projectTitle: "Naa-Kaai-Kai-Enterprise",
+    projectDescription:
+      "This Project is for a web application on a company call Naa-Kaai-Kai Enterprise is a business venture focused on providing high-quality products and services tailored to meet customer needs. The company emphasizes customer satisfaction, innovative solutions, and a commitment to excellence in every aspect of its operations",
+    technologiesUsed: ["React", "Vanilla CSS", "JavaScript"],
+    liveDemo: "https://naa-kaai-kai-enterprise.vercel.app/",
+    code: "https://github.com/mulbahoplanojames/SteveCom",
+  },
+  {
+    projectID: 3,
+    image: Dorcas_school,
+    projectTitle: "Dorcas Christain School",
+    projectDescription:
+      "This Project is for a web application the allow client and to unit with other business partners to buy products and view some of the latest blog posts..",
+    technologiesUsed: ["React", "Vanilla CSS", "JavaScript"],
+    liveDemo: "https://naa-kaai-kai-enterprise.vercel.app/",
+    code: "https://github.com/mulbahoplanojames/Dorcas_Christain_School",
+  },
+  {
+    projectID: 4,
+    image: BoldImg,
+    projectTitle: "Boldo Company website",
+    projectDescription:
+      "This Project is for a web application the allow client and to unit with other business partners to buy products and view some of the latest blog posts..",
+    technologiesUsed: ["React", "Vanilla CSS", "JavaScript"],
+    liveDemo: "https://boldo-com.vercel.app/",
+    code: "https://github.com/mulbahoplanojames/Boldo.com",
+  },
+  {
+    projectID: 5,
+    image: BoldImg,
+    projectTitle: "Boldo Company website",
+    projectDescription:
+      "This Project is for a web application the allow client and to unit with other business partners to buy products and view some of the latest blog posts..",
+    technologiesUsed: ["React", "Vanilla CSS", "JavaScript"],
+    liveDemo: "https://boldo-com.vercel.app/",
+    code: "https://github.com/mulbahoplanojames/Boldo.com",
+  },
+  {
+    projectID: 6,
+    image: DigitStemImg,
+    projectTitle: "DigitStem Clone Landing Page",
+    projectDescription:
+      "This project is a Front-end clone of the Digitstem platform that connects vendors to buyers through active affiliates. It offers various features and services aimed at facilitating online business transactions.",
+    technologiesUsed: ["HTML", "Vanilla CSS", "JavaScript"],
+    liveDemo: "https://mulbahoplanojames.github.io/Digitstem-Clone/",
+    code: "https://github.com/mulbahoplanojames/Digitstem-Clone",
+  },
+  {
+    projectID: 7,
+    image: Math4kidsImg,
+    projectTitle: "Math 4 Kids, a math game for Kids",
+    projectDescription:
+      "This is a web application designed to help kids learn Mathematics in a fun and interactive way. This project makes kids to learn mathematics in an easy and simple way. The project utilizes HTML, CSS, and JavaScript.",
+    technologiesUsed: ["HTML", "Vanilla CSS", "Vanilla JavaScript"],
+    liveDemo: "https://mulbahoplanojames.github.io/Math-4-Kids/",
+    code: "https://github.com/mulbahoplanojames/Math-4-Kids",
+  },
 
-	{
-		projectID: 5,
-		image: BinageImg,
-		projectTitle: "Binage!9 web Application",
-		projectDescription:
-			"This is a Front-end project for a web app that for a virtual company that is involve in developing web site and web applications for clients for a reasonable price and their work is done effectively",
-		technologiesUsed: ["React", "Css Modules", "React Syyled Components"],
-		liveDemo: "https://binage9.netlify.app/",
-		code: "https://github.com/mulbahoplanojames/Binage-9",
-	},
+  {
+    projectID: 8,
+    image: BinageImg,
+    projectTitle: "Binage!9 web Application",
+    projectDescription:
+      "This is a Front-end project for a web app that for a virtual company that is involve in developing web site and web applications for clients for a reasonable price and their work is done effectively",
+    technologiesUsed: ["React", "Css Modules", "React Syyled Components"],
+    liveDemo: "https://binage9.netlify.app/",
+    code: "https://github.com/mulbahoplanojames/Binage-9",
+  },
 ];
 
 export default ABOUT_ME;
